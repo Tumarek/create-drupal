4a0f10aec1a1f9d87bd9c6a15d4d9031f36a867f
Paul Bartels <paul.bartels@berger-schmidt.de>
2017-04-20 10:54:17 +0200

    Added File Permissions script

---
+++ file_permissions.sh
@@ -0,0 +1,10 @@
+chown -R dev:www-data .
+find . -type d -exec chmod u=rwxs,g=rxs,o= '{}' \;
+find . -type f -exec chmod u=rw,g=r,o= '{}' \;
+cd sites
+find . -type d -name files -exec chmod ug=rwxs,o= '{}' \;
+  for d in ./*/files
+    do
+      find $d -type d -exec chmod ug=rwxs,o= '{}' \;
+      find $d -type f -exec chmod ug=rw,o= '{}' \;
+    done
